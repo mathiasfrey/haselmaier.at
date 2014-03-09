5480cecd2b1bbc8e1815d7388e57642d3509bbc4
Mathias Frey <frey@wu.ac.at>
2014-03-09 10:06:44 +0100

    added setup.py to be installable via pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='haselmaier.at',
+    version='0.1',
+    description='website of haselmaier.at',
+    author='Mathias Frey',
+    packages = [],
+)
