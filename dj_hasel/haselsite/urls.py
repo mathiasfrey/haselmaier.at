9c9c32a3a06172ce46dd29178f606ce118ee9185
Mathias Frey <frey@wu.ac.at>
2014-03-25 15:34:16 +0100

    fixes: Could not parse the remainder: '-art' from 'wohn-art'. The syntax of 'url' changed in Django 1.5, see the docs.

--- dj_hasel/haselsite/urls.py
+++ dj_hasel/haselsite/urls.py
@@ -8,12 +8,12 @@ urlpatterns = patterns('',
     url(r'^group/$', 'haselsite.views.group', name='group'),
     url(r'^tischlerei/$', 'haselsite.views.group', name='tischlerei'),
     
-    url(r'^wohn-art/$', 'haselsite.views.wohn_art', name='wohn-art'),
+    url(r'^wohn-art/$', 'haselsite.views.wohn_art', name='wohn_art'),
     url(r'^wohnart/$', RedirectView.as_view(url='/wohn-art/', permanent=False)),
     
     url(r'^leitstellen/$', 'haselsite.views.group', name='leitstellen'),
     
-    url(r'^e-technik/$', 'haselsite.views.group', name='e-technik'),
+    url(r'^e-technik/$', 'haselsite.views.group', name='e_technik'),
     url(r'^etechnik/$', RedirectView.as_view(url='/e-technik/', permanent=False)),
  
 )
