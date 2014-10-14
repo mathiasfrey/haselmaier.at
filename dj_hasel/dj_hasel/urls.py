e44ea4a3aa503d32b574636e10f16e252d4b0d29
Gabor Guzmics <gab@g4b.org>
2014-10-14 22:38:37 +0200

    settings template update for gallery, urls py (admin enabled shortly, added gal), fix gallery-list include

--- dj_hasel/dj_hasel/urls.py
+++ dj_hasel/dj_hasel/urls.py
@@ -4,7 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       
+    url(r'^gal/', include('gal.urls')),
+    # Uncomment the next line to enable the admin:
+    url(r'^_admin/', include(admin.site.urls)),
+    
     url(r'^', include('haselsite.urls')),
     
 
