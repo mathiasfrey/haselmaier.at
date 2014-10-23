dffd9c99168b7f611b2d7129c9885d40f0fc9101
Gabor Guzmics <gab@g4b.org>
2014-10-23 18:08:14 +0200

    retrieve pictures tag, modified category preview to use it as an example

---
+++ dj_hasel/gal/templatetags/gal.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+from models import Gallery, Category, Picture
+register = template.Library()
+
+@register.assignment_tag
+def get_category_pictures(category, gallery=None):
+    # if gallery is given, we search there.
+    if gallery is not None:
+        gal = None
+        if isinstance(gallery, basestring):
+            # strings go to slugs
+            try:
+                gal = Gallery.objects.get(slug=gallery)
+            except:
+                pass
+        else:
+            try:
+                gal = Gallery.objects.get(pk=gallery)
+            except:
+                pass
+        if gal is None:
+            return ''
+        try:
+            cat = Category.objects.get(slug=category,
+                                       gallery=gal)
+        except:
+            return ''
+    else:
+        try:
+            cat = Category.objects.get(slug=category)
+        except:
+            return ''
+    return Picture.objects.filter(category=cat)
