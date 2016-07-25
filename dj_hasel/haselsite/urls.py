48a0ad632a76ee443336214a5d320dc023c2e000
Mathias Frey <mathias.c.frey@gmail.com>
2016-07-25 11:23:14 +0200

    changed "blog" to "stories"

--- dj_hasel/haselsite/urls.py
+++ dj_hasel/haselsite/urls.py
@@ -17,13 +17,24 @@ urlpatterns = patterns('',
     url(r'^e-technik/$', 'haselsite.views.e_technik', name='e_technik'),
     url(r'^etechnik/$', RedirectView.as_view(url='/e-technik/', permanent=False)),
     
-    url(r'^blog/$', 'haselsite.views.blog', name='blog'),
+    # stories and blog redirect
+    url(r'^stories/$', 'haselsite.views.blog', name='blog'),
+    #url(r'^blog/$', RedirectView.as_view(url='/stories/', permanent=False)),
     
-    url(r'^blog/autor/(?P<id>[\d]+)/$', 'haselsite.views.blog_author', name='blog_author'),
-    url(r'^blog/schlagwort/(?P<tag>[\w-]+)/$', 'haselsite.views.blog_tag', name='blog_tag'),
-    url(r'^blog/organisation/(?P<brand>[\w-]+)/$', 'haselsite.views.blog_brand', name='blog_brand'),
+    # stories and blog redirect
+    url(r'^stories/autor/(?P<id>[\d]+)/$', 'haselsite.views.blog_author', name='blog_author'),
+    url(r'^stories/schlagwort/(?P<tag>[\w-]+)/$', 'haselsite.views.blog_tag', name='blog_tag'),
+    url(r'^stories/organisation/(?P<brand>[\w-]+)/$', 'haselsite.views.blog_brand', name='blog_brand'),
     
-    url(r'^blog/(?P<slug>[\w-]+)/$', 'haselsite.views.blog_detail', name='blog_detail'),
- 
-    url(r'^test/$', 'haselsite.views.test', name='test'), 
+    #url(r'^blog/autor/(?P<id>[\d]+)/$', RedirectView.as_view(url=r'^blog/autor/(?P<id>[\d]+)/$', permanent=False)),
+    #url(r'^blog/schlagwort/(?P<tag>[\w-]+)/$', RedirectView.as_view(url=r'^blog/schlagwort/(?P<tag>[\w-]+)/$', permanent=False)),
+    #url(r'^blog/organisation/(?P<brand>[\w-]+)/$', RedirectView.as_view(url=r'^blog/organisation/(?P<brand>[\w-]+)/$', permanent=False)),
+    
+    # stories and blog redirect
+    url(r'^stories/(?P<slug>[\w-]+)/$', 'haselsite.views.blog_detail', name='blog_detail'),
+    #url(r'^blog/(?P<slug>[\w-]+)/$', RedirectView.as_view(url=r'/stories/(?P<slug>[\w-]+)/', permanent=False)),
+    
+    url(r'^test/$', 'haselsite.views.test', name='test'),
+    
+    url(r'^blog/(?P<path>.*)', RedirectView.as_view(url='/stories/%(path)s', permanent=False)),
 )
