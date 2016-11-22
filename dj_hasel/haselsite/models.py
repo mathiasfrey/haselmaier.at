f8585fd26062eaf5742bc4feb885865ead7bb667
Mathias Frey <mathias.c.frey@gmail.com>
2016-11-22 19:02:33 +0100

    added unicode support for blog entry model

--- dj_hasel/haselsite/models.py
+++ dj_hasel/haselsite/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django.utils.encoding import python_2_unicode_compatible
 
 class BlogAuthor(models.Model):
     
@@ -10,7 +11,8 @@ class BlogAuthor(models.Model):
     def __str__(self):
         return self.name
     
-    
+
+@python_2_unicode_compatible 
 class BlogEntry(models.Model):
     
     GROUP = 'GR'
